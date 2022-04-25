e275d52f16eb7ea90f0c340438aa7c67db698987
Guillermo García López <memogl25@gmail.com>
2022-04-24 23:46:31 -0500

    Add hooks.

--- test/cypress/integration/login.spec.js
+++ test/cypress/integration/login.spec.js
@@ -1,12 +1,12 @@
 'use strict'
 
 describe('Pruebas del login', () => {
-  it('Debe cargar el login', () => {
+  beforeEach(() => {
     cy.visit('/login')
+    cy.contains('h1', 'Bienvenido').should('be.visible')
   })
 
-  it('Debe registrar un usuario', () => {
-    cy.visit('/login')
+  it.skip('Debe registrar un usuario', () => {
     cy.contains('Crear una cuenta').click()
     cy.get('#name').type('Usuario de prueba')
     cy.get('#title').type('Compañia de prueba')
@@ -18,7 +18,6 @@ describe('Pruebas del login', () => {
   })
 
   it('Debe fallar con un usuario erroneo.', () => {
-    cy.visit('/login')
     cy.get('#email1').type('fail@test.com')
     cy.get('#password1').type('test1234')
     cy.contains('.button', 'Ingresar').click()
@@ -27,11 +26,14 @@ describe('Pruebas del login', () => {
   })
 
   it('Debe loguear un usuario', () => {
-    cy.visit('/login')
     cy.get('#email1').type('test@test.com')
     cy.get('#password1').type('test1234')
     cy.contains('.button', 'Ingresar').click()
     cy.wait(3000)
     cy.contains('a', 'Dashboard').should('be.visible')
+  })
+
+  after(() => {
+    cy.log('Test finalizados')
   })
 })
