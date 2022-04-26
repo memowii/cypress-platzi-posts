db053c9e4b09f3c1f8f145e0665d56a15b90b600
Guillermo García López <memogl25@gmail.com>
2022-04-25 19:25:16 -0500

    Variables, Fixtures y Alias

--- test/cypress/integration/login.spec.js
+++ test/cypress/integration/login.spec.js
@@ -2,19 +2,22 @@
 
 describe('Pruebas del login', () => {
   beforeEach(() => {
+    cy.fixture('user.json').as('userData')
     cy.visit('/login')
     cy.contains('h1', 'Bienvenido').should('be.visible')
   })
 
   it.skip('Debe registrar un usuario', () => {
-    cy.contains('Crear una cuenta').click()
-    cy.get('#name').type('Usuario de prueba')
-    cy.get('#title').type('Compañia de prueba')
-    cy.get('#email2').type('test@test.com')
-    cy.get('#password2').type('test1234')
-    cy.contains('.button', 'Registrarse').click()
-    cy.wait(3000)
-    cy.get('.error-msg').should('not.exist')
+    cy.get('@userData').then((userData) => {
+      cy.contains('Crear una cuenta').click()
+      cy.get('#name').type(userData.name)
+      cy.get('#title').type(userData.company)
+      cy.get('#email2').type(userData.email)
+      cy.get('#password2').type(userData.password)
+      cy.contains('.button', 'Registrarse').click()
+      cy.wait(3000)
+      cy.get('.error-msg').should('not.exist')
+    })
   })
 
   it('Debe fallar con un usuario erroneo.', () => {
@@ -26,11 +29,13 @@ describe('Pruebas del login', () => {
   })
 
   it('Debe loguear un usuario', () => {
-    cy.get('#email1').type('test@test.com')
-    cy.get('#password1').type('test1234')
-    cy.contains('.button', 'Ingresar').click()
-    cy.wait(3000)
-    cy.contains('a', 'Dashboard').should('be.visible')
+    cy.get('@userData').then((userData) => {
+      cy.get('#email1').type(userData.email)
+      cy.get('#password1').type(userData.password)
+      cy.contains('.button', 'Ingresar').click()
+      cy.wait(3000)
+      cy.contains('a', 'Dashboard').should('be.visible')
+    })
   })
 
   after(() => {
