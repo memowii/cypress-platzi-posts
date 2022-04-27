58900b9d88e8962fed31c289fd282ed29b8b9cd6
Guillermo García López <memogl25@gmail.com>
2022-04-26 21:38:35 -0500

    Comandos personalizados

--- test/cypress/integration/login.spec.js
+++ test/cypress/integration/login.spec.js
@@ -13,31 +13,18 @@ describe('Pruebas del login', () => {
 
   it('Debe registrar un usuario', () => {
     cy.get('@userData').then((userData) => {
-      cy.contains('Crear una cuenta').click()
-      cy.get('#name').type(userData.name)
-      cy.get('#title').type(userData.company)
-      cy.get('#email2').type(userData.email)
-      cy.get('#password2').type(userData.password)
-      cy.contains('.button', 'Registrarse').click()
-      cy.wait(3000)
-      cy.get('.error-msg').should('not.exist')
+      cy.createUser(userData)
     })
   })
 
   it('Debe fallar con un usuario erroneo.', () => {
-    cy.get('#email1').type('fail@test.com')
-    cy.get('#password1').type('test1234')
-    cy.contains('.button', 'Ingresar').click()
-    cy.wait(3000)
+    cy.loginUser('fail@test.com', 'test1234')
     cy.get('.error-msg').should('be.visible')
   })
 
   it('Debe loguear un usuario', () => {
     cy.get('@userData').then((userData) => {
-      cy.get('#email1').type(userData.email)
-      cy.get('#password1').type(userData.password)
-      cy.contains('.button', 'Ingresar').click()
-      cy.wait(3000)
+      cy.loginUser(userData.email, userData.password)
       cy.contains('a', 'Dashboard').should('be.visible')
     })
   })
